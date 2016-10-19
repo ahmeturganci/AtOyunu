8c9c163a5aea988860264b21b8eeea91c4c7673c
AhmetU <aahmet.272@gmail.com>
2016-10-19 18:15:20 +0300

    Proje dosyaları yüklendi

---
+++ AtOyunu/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace AtOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPanel = new System.Windows.Forms.DataGridView();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPanel
+            // 
+            this.dgvPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPanel.Location = new System.Drawing.Point(12, 12);
+            this.dgvPanel.Name = "dgvPanel";
+            this.dgvPanel.Size = new System.Drawing.Size(18, 20);
+            this.dgvPanel.TabIndex = 0;
+            this.dgvPanel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPanel_CellClick);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(396, 17);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "5x5";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(444, 12);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(75, 27);
+            this.btnOlustur.TabIndex = 2;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 370);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.dgvPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPanel;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btnOlustur;
+    }
+}
+
---
+++ AtOyunu/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtOyunu
+{
+    public partial class Form1 : Form
+    {
+        int X, Y;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Kontrol(int x, int y)
+        {
+            //kontrol
+
+            if (y + 1 <= Y && x - 2 >= X)
+                dgvPanel.Rows[y + 1].Cells[x - 2].Value = "a";
+
+            if (y + 1 <= Y && x + 2 <= X)
+                dgvPanel.Rows[y + 1].Cells[x + 2].Value = "a";
+
+            if (y - 1 >= 0 && x + 2 <= X)
+                dgvPanel.Rows[y - 1].Cells[x + 2].Value = "a";
+
+            if (y - 1 >= 0 && x - 2 >= 0)
+                dgvPanel.Rows[y - 1].Cells[x - 2].Value = "a";
+
+            if (y - 2 >= 0 && x + 1 <= X)
+                dgvPanel.Rows[y - 2].Cells[x + 1].Value = "a";
+
+            if (y - 2 >= 0 && x - 1 >= 0)
+                dgvPanel.Rows[y - 2].Cells[x - 1].Value = "a";
+
+            if (y + 2 <= Y && x - 1 >= 0)
+                dgvPanel.Rows[y + 2].Cells[x - 1].Value = "a";
+
+            if (y + 2 <= Y && x + 1 <= X)
+                dgvPanel.Rows[y + 2].Cells[x + 1].Value = "a";
+
+
+
+
+
+        }
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(X.ToString());
+            DataTable dt = new DataTable();
+            for (int i = 0; i < X; i++)
+            {
+                dt.Columns.Add();
+            }
+            for (int i = 0; i < Y; i++)
+            {
+                dt.Rows.Add();
+            }
+            // bos hatalı şuan
+
+            dgvPanel.DataSource = dt;
+            foreach (DataGridViewRow row in dgvPanel.Rows)
+            {
+                row.Height = 50;
+
+            }
+            foreach (DataGridViewColumn col in dgvPanel.Columns)
+            {
+                col.Width = 50;
+
+
+
+                dgvPanel.ColumnHeadersVisible = false;
+                dgvPanel.RowHeadersVisible = false;
+                dgvPanel.DefaultCellStyle.SelectionBackColor = dgvPanel.DefaultCellStyle.BackColor;
+            }
+            //dgvPanel.Rows[curY].Cells[curX].Value = yildiz;
+
+            dgvPanel.ColumnHeadersVisible = false;
+            dgvPanel.RowHeadersVisible = false;
+
+            dgvPanel.AutoSize = true;
+
+            // arr = komut.ToCharArray();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPanel_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show(dgvPanel.CurrentCellAddress.X.ToString() + " " + dgvPanel.CurrentCellAddress.Y.ToString());
+            Kontrol(dgvPanel.CurrentCellAddress.X, dgvPanel.CurrentCellAddress.Y);
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 5;
+            Y = 5;
+        }
+    }
+}
