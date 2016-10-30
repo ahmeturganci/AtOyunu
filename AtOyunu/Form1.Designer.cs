9f0ab562174c23f0846a9a226a9c01de05d4e2ca
mehmetgelmedi <mehmetemingelmedi@gmail.com>
2016-10-30 20:55:20 +0200

    Gelişmiş kontrol mekanizması tamamlandı, Skor tamamlandı,Form ve Panel düzenlendi,Bazı açıklar kapatıldı.

--- AtOyunu/Form1.Designer.cs
+++ AtOyunu/Form1.Designer.cs
@@ -28,38 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvPanel = new System.Windows.Forms.DataGridView();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.btnOlustur = new System.Windows.Forms.Button();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.dgvPanel = new System.Windows.Forms.DataGridView();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvPanel
-            // 
-            this.dgvPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPanel.Location = new System.Drawing.Point(12, 12);
-            this.dgvPanel.Name = "dgvPanel";
-            this.dgvPanel.Size = new System.Drawing.Size(18, 20);
-            this.dgvPanel.TabIndex = 0;
-            this.dgvPanel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPanel_CellClick);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(396, 17);
+            this.radioButton2.Location = new System.Drawing.Point(12, 44);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "5x5";
+            this.radioButton2.Text = "5 x 5";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // btnOlustur
             // 
-            this.btnOlustur.Location = new System.Drawing.Point(444, 12);
+            this.btnOlustur.Location = new System.Drawing.Point(12, 11);
             this.btnOlustur.Name = "btnOlustur";
             this.btnOlustur.Size = new System.Drawing.Size(75, 27);
             this.btnOlustur.TabIndex = 2;
@@ -70,39 +64,94 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(396, 40);
+            this.radioButton3.Location = new System.Drawing.Point(12, 67);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(42, 17);
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
             this.radioButton3.TabIndex = 3;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "6x6";
+            this.radioButton3.Text = "6 x 6";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(396, 63);
+            this.radioButton4.Location = new System.Drawing.Point(12, 90);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(42, 17);
+            this.radioButton4.Size = new System.Drawing.Size(48, 17);
             this.radioButton4.TabIndex = 4;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "7x7";
+            this.radioButton4.Text = "7 x 7";
             this.radioButton4.UseVisualStyleBackColor = true;
             this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // dgvPanel
+            // 
+            this.dgvPanel.AllowUserToAddRows = false;
+            this.dgvPanel.AllowUserToDeleteRows = false;
+            this.dgvPanel.AllowUserToResizeColumns = false;
+            this.dgvPanel.AllowUserToResizeRows = false;
+            this.dgvPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPanel.Location = new System.Drawing.Point(105, 11);
+            this.dgvPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvPanel.MultiSelect = false;
+            this.dgvPanel.Name = "dgvPanel";
+            this.dgvPanel.Size = new System.Drawing.Size(30, 30);
+            this.dgvPanel.TabIndex = 8;
+            this.dgvPanel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPanel_CellClick);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(12, 159);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(60, 17);
+            this.radioButton7.TabIndex = 7;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "10 x 10";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 113);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(48, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "8 x 8";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(12, 136);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(48, 17);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "9 x 9";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 370);
+            this.ClientSize = new System.Drawing.Size(597, 370);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.dgvPanel);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.btnOlustur);
             this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.dgvPanel);
             this.Name = "Form1";
             this.Text = "At Oyunu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,11 +159,14 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvPanel;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Button btnOlustur;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.DataGridView dgvPanel;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
 
--- AtOyunu/Form1.cs
+++ AtOyunu/Form1.cs
@@ -40,83 +40,101 @@ namespace AtOyunu
             }
             catch
             {
-                s = null; // satır sütun olayından dolayı patlama var burada
+                s = null;
                 sayiMi = true;
             }
 
-            
-           
             return sayiMi;
         }
         public void Kontrol(int x, int y)
         {
             //tanımlama
-
-            Temizle();
+            int[] skorKontrol = new int[8];
+            int sayac = 0;
 
             //kontrol
             //
+            Temizle();
             if (y + 1 <= Y && x - 2 >= 0)
             {
                 if (!sayiMi(x - 2, y + 1))
+                {
                     dgvPanel.Rows[y + 1].Cells[x - 2].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[0] = 1;
+                }
             }
             if (y + 1 <= Y && x + 2 <= X)
             {
                 if (!sayiMi(x + 2, y + 1))
+                {
                     dgvPanel.Rows[y + 1].Cells[x + 2].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[1] = 1;
+                }
             }
             if (y - 1 >= 0 && x + 2 <= X)
             {
                 if (!sayiMi(x + 2, y - 1))
+                {
                     dgvPanel.Rows[y - 1].Cells[x + 2].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[2] = 1;
+                }
             }
             if (y - 1 >= 0 && x - 2 >= 0)
             {
                 if (!sayiMi(x - 2, y - 1))
+                {
                     dgvPanel.Rows[y - 1].Cells[x - 2].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[3] = 1;
+                }
             }
             if (y - 2 >= 0 && x + 1 <= X)
             {
                 if (!sayiMi(x + 1, y - 2))
-                    dgvPanel.Rows[y - 2].Cells[x + 1].Style.BackColor = Color.PaleVioletRed;
+                {
+                    dgvPanel.Rows[y - 2].Cells[x + 1].Style.BackColor = Color.PaleVioletRed; 
+                    skorKontrol[4] = 1;
+                }
             }
             if (y - 2 >= 0 && x - 1 >= 0)
             {
                 if (!sayiMi(x - 1, y - 2))
+                {
                     dgvPanel.Rows[y - 2].Cells[x - 1].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[5] = 1;
+                }
             }
             if (y + 2 <= Y && x - 1 >= 0)
             {
                 if (!sayiMi(x - 1, y + 2))
+                {
                     dgvPanel.Rows[y + 2].Cells[x - 1].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[6] = 1;
+                }
             }
             if (y + 2 <= Y && x + 1 <= X)
             {
                 if (!sayiMi(x + 1, y + 2))
+                {
                     dgvPanel.Rows[y + 2].Cells[x + 1].Style.BackColor = Color.PaleVioletRed;
+                    skorKontrol[7] = 1;
+                }
             }
 
-
-
-            // dizi elemanlarının hepsi sıfır ise oyun biter skoru ekrana yazdır...
-            //for (int i = 0; i < skorKontrol.Length; i++)
-            //{
-            //    if (skorKontrol[i] == 0)
-            //        sayac++;
-            //}
-            //if (sayac == 7)
-            //{
-            //    if (skor != x * y)
-            //        MessageBox.Show("Oyun Bitti.\n Skorunuz : " + skor.ToString(), "Oyun");
-            //}
+            //dizi elemanlarının hepsi sıfır ise oyun biter skoru ekrana yazdır...
+            for (int i = 0; i < 8; i++)
+            {
+                if (skorKontrol[i] == 0)
+                    sayac++;
+            }
+            if (sayac >= 8)
+                MessageBox.Show("Oyun Bitti.\n Skorunuz : " + (skor - 1).ToString(), "Oyun");
         }
         private void btnOlustur_Click(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
-            for (int i = 0; i <= X; i++)
+            for (int i = 0; i < X; i++)
                 dt.Columns.Add();
-            for (int j = 0; j <Y; j++)
+            for (int j = 0; j < Y; j++)
                 dt.Rows.Add();
 
             dgvPanel.DataSource = dt;
@@ -136,51 +154,64 @@ namespace AtOyunu
             dgvPanel.AutoSize = true;
         }
 
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 5;
+            Y = 5;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 6;
+            Y = 6;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 7;
+            Y = 7;
+        }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 8;
+            Y = 8;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 9;
+            Y = 9;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 10;
+            Y = 10;
+        }
+
         private void dgvPanel_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            //int tempX = -1, tempY = -1;
+            //int tempX = -2, tempY = -2;
             curX = dgvPanel.CurrentCellAddress.X;
             curY = dgvPanel.CurrentCellAddress.Y;
-            //if(tempX==curX && tempY==curY)
-            //    MessageBox.Show("Seçilemez");
+            //if (tempX == curX && tempY == curY)
+            //    MessageBox.Show("Seçilemez!", "Oyun");
             //tempX = curX;
             //tempY = curY;
-            MessageBox.Show(curX.ToString() + " " + curY.ToString());
-            if (k == 0)
+            //x ve y adres değerleri ekrana yazdir => MessageBox.Show(curX.ToString() + " " + curY.ToString());
+            if (k == 0) // program ilk çalıştığında
             {
                 dgvPanel.Rows[curY].Cells[curX].Value = skor;
                 skor++;
                 Kontrol(curX, curY);
                 k++;
             }
-            if (dgvPanel.Rows[curY].Cells[curX].Style.BackColor == Color.PaleVioletRed && !sayiMi(curX,curY))
+            if (dgvPanel.Rows[curY].Cells[curX].Style.BackColor == Color.PaleVioletRed && !sayiMi(curX, curY))
             {
                 dgvPanel.Rows[curY].Cells[curX].Value = skor;
                 skor++;
                 Kontrol(curX, curY);
             }
-            else
-            {
-                    MessageBox.Show("Seçilemez!");
-            }
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            X = 4;
-            Y = 4;
-        }
-
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            X = 5;
-            Y = 5;
-        }
-
-        private void radioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-            X = 6;
-            Y = 6;
         }
     }
 }
